06b7234f8709fab4eee5fea69bd37ae16641f11a
Joseph Chan <jchan24@uoguelph.ca>
2018-11-30 14:57:33 -0500

    more code for tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,22 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+exports.onRouteUpdate = function({ location }) {
+    // Don't track while developing.
+    if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
+      if (
+        location &&
+        typeof window.excludeGAPaths !== `undefined` &&
+        window.excludeGAPaths.some(rx => rx.test(location.pathname))
+      ) {
+        return
+      }
+      window.ga(
+        `set`,
+        `page`,
+        location ? location.pathname + location.search + location.hash : undefined
+      )
+      window.ga(`send`, `pageview`)
+    }
+  }
 // You can delete this file if you're not using it
